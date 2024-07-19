2e0129616d577320b149a6e9072bfa2bb6956ccd
GhadaAdel <gh.adel.aa@gmail.com>
2024-07-19 23:14:48 +0300

    createNoteForm

---
+++ routes.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    '/Demo/index' => 'controllers/index.php',
+    '/Demo/about' => 'controllers/about.php',
+    '/Demo/notes' => 'controllers/notes.php',
+    '/Demo/note' => 'controllers/note.php',
+    '/Demo/note/create' => 'controllers/note-create.php',
+];
+
+?>
