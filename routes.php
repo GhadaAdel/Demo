ee36372e7359efbd5f1adf4558546d106bd28166
GhadaAdel <gh.adel.aa@gmail.com>
2024-07-29 00:06:13 +0300

    extract validation object

--- routes.php
+++ routes.php
@@ -1,25 +1,25 @@
 <?php
 
-$router->get('/Demo/index', 'controllers/index.php');
-$router->get('/Demo/about', 'controllers/about.php');
-$router->get('/Demo/notes', 'controllers/notes/index.php')->only('auth');
+$router->get('/Demo/index', 'index.php');
+$router->get('/Demo/about', 'about.php');
+$router->get('/Demo/notes', 'notes/index.php')->only('auth');
 
-$router->get('/Demo/note', 'controllers/notes/show.php');
-$router->get('/Demo/note/create', 'controllers/notes/create.php');
-$router->delete('/Demo/note', 'controllers/notes/destroy.php');
+$router->get('/Demo/note', 'notes/show.php');
+$router->get('/Demo/note/create', 'notes/create.php');
+$router->delete('/Demo/note', 'notes/destroy.php');
 
-$router->get('/Demo/note/edit', 'controllers/notes/edit.php');
-$router->patch('/Demo/note', 'controllers/notes/update.php');
+$router->get('/Demo/note/edit', 'notes/edit.php');
+$router->patch('/Demo/note', 'notes/update.php');
 
 //common restful conventions: make a post request to the notes resource
-$router->post('/Demo/notes', 'controllers/notes/store.php'); 
+$router->post('/Demo/notes', 'notes/store.php'); 
 
-$router->get('/Demo/register', 'controllers/registration/create.php')->only('guest');
+$router->get('/Demo/register', 'registration/create.php')->only('guest');
 
-$router->post('/Demo/register', 'controllers/registration/store.php');
+$router->post('/Demo/register', 'registration/store.php');
 
-$router->get('/Demo/login', 'controllers/sessions/create.php');
-$router->post('/Demo/login', 'controllers/sessions/store.php');
-$router->delete('/Demo/logout', 'controllers/sessions/destroy.php')->only('auth');
+$router->get('/Demo/login', 'sessions/create.php');
+$router->post('/Demo/login', 'sessions/store.php');
+$router->delete('/Demo/logout', 'sessions/destroy.php')->only('auth');
 
 ?>
