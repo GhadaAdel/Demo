8f2a0796ec5bfee8c2ed08dec9dc68d53b96ed21
GhadaAdel <gh.adel.aa@gmail.com>
2024-07-30 03:04:11 +0300

    PRG Pattern - Session Flashing

--- Http/forms/Authenticator.php
+++ Http/forms/Authenticator.php
@@ -3,6 +3,7 @@
 namespace Http\forms;
 use Core\Database;
 use Core\App;
+use Core\Session;
 
 class Authenticator
 {
@@ -38,10 +39,6 @@ class Authenticator
 
     public function logout()
     {
-        $_SESSION = [];
-        session_destroy();
-
-        $params = session_get_cookie_params();
-        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+        Session::destroy();
     }
 }
